d174895d9d1b987e00df0fc3779f685887e42657
Will Yang <wkyjyy@gmail.com>
2014-05-11 20:52:33 +0800

    pretty-print and error indication

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 SRC = syntax.mli syntax.ml parser.mli parser.ml lexer.ml main.ml
 
 all: $(SRC)
-	ocamlfind ocamlopt -linkpkg -package batteries -o checker $(SRC)
+	ocamlopt -o checker $(SRC)
 
 parser.mli parser.ml:
 	ocamlyacc parser.mly
@@ -14,7 +14,7 @@ lexer.ml:
 .SUFFIXES: .ml .cmo .mli .cmi
 
 .ml.cmo:
-	ocamlfind ocamlc -linkpkg -package batteries $(SRC)
+	ocamlc $(SRC)
 
 clean:
 	rm *.cmi *.cmx *.o *.cmo parser.ml parser.mli lexer.ml
--- main.ml
+++ main.ml
@@ -1,12 +1,30 @@
 (* main.ml *)
-open Batteries
+open Syntax;;
+
+let rec value_to_str value :string = match value with
+  Unit -> "Unit"
+| Var(v) -> v
+| Bool(b) -> string_of_bool b
+| Int(i) -> string_of_int i
+| Op(op, v) -> op ^ (value_to_str v)
+| Pair(v1, v2) -> "("^(value_to_str v1)^", "^(value_to_str v2)^")"
+
+let rec proc_to_str proc :string = match proc with 
+  Zero -> "Zero"
+| Par(p1, p2) -> "Parallel("^(proc_to_str p1)^", "^(proc_to_str p2)^")"
+| Rep(p) -> "Repeat("^(proc_to_str p)^")"
+| Nu(var, p) -> "New("^var^", "^(proc_to_str p)^")"
+| If(cond, p1, p2) -> "If ["^(value_to_str cond)^" Then "^(proc_to_str p1)^" Else "^(proc_to_str p2)
+| In(chan, var, p) -> "In "^chan^"("^var^")."^(proc_to_str p)
+| Out(chan, value, p) -> "Out "^chan^"("^(value_to_str value)^")."^(proc_to_str p)
+
 
 let main () = 
 	let _ = print_string "Pi-Calculus Type Checker\n" in
 	let filename = 
 		try Sys.argv.(1)
 		with Invalid_argument _ -> (
-			print_string "Usage:\n  checker <file>\n"; 
+			print_string "Usage:  checker <file>\n"; 
 			exit(-1)) in
 	let in_strm = 
 		try open_in filename
@@ -14,11 +32,8 @@ let main () =
 			print_string ("Cannot open file: "^filename^"\n"); 
 			exit(-1)) in
 	let lex_buf = Lexing.from_channel in_strm in
-	let proc =
-		try Parser.main Lexer.token lex_buf
-		with 
-		  Failure _ -> exit(-1)
-		| Parsing.Parse_error -> exit(-1)
-	in print_string ((dump proc)^"\n");;
+	let proc = try Parser.main Lexer.token lex_buf 
+		with exn -> ( print_endline ("error at character "^(string_of_int lex_buf.Lexing.lex_curr_p.pos_cnum)); exit(-1))
+	in print_string ((proc_to_str proc)^"\n");;
 
 if !Sys.interactive then () else main();;
